4a84ec90873c59cc7ce5c7c54679be66e23bdf66
bdgroves <bdgroves1970@gmail.com>
2023-11-16 16:53:44 -0800

    working with some phospahate data around Lake Tahoe

--- 2023/01_points_usgs/day_1_points_nv_phos.R
+++ 2023/01_points_usgs/day_1_points_nv_phos.R
@@ -3,6 +3,7 @@ library(ggsn)
 library(sf)
 library(dplyr)
 library(dataRetrieval)
+library(leaflet)
 
 NV_sites <- whatNWISsites(stateCd = "NV", 
                           parameterCd = "00665")
@@ -24,3 +25,69 @@ ggplot() +
   scalebar(usa[ usa$ID == "nevada" ,],
            dist=100, dist_unit="mi", st.size = 3,
            transform=TRUE, model="WGS84")
+
+pCode <- c("00665")
+phWI <- whatNWISdata(stateCd="NV", 
+                     parameterCd=pCode)
+
+phWI.1 <- phWI %>% 
+  filter(count_nu > 300) %>%
+  mutate(period = as.Date(end_date) - as.Date(begin_date)) %>%
+  filter(period > 15*365)
+
+phos_NV_data <- readNWISqw(siteNumbers = phWI.1$site_no,
+                           parameterCd = pCode)
+
+phos_summary <- phos_NV_data %>% 
+  group_by(site_no) %>% 
+  summarize(max = max(result_va, na.rm = TRUE),
+            count = n()) %>% 
+  ungroup() %>% 
+  left_join(attr(phos_NV_data, "siteInfo"), 
+            by = "site_no")
+
+# Assuming phos_summary has latitude and longitude columns (replace with your actual column names)
+phos_summary_map <- phos_summary %>%
+  filter(!is.na(dec_lat_va) & !is.na(dec_long_va)) %>%
+  leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap tiles
+  addCircleMarkers(
+    lat = ~dec_lat_va,  # Specify the latitude column
+    lng = ~dec_long_va,  # Specify the longitude column
+    radius = ~sqrt(count),  # Adjust the circle size based on count
+    color = "blue",         # Circle color
+    fillOpacity = 0.7,       # Circle fill opacity
+    label = ~paste("Site: ", site_no, "<br>Max Phos: ", max),
+    popup = ~paste("Site: ", site_no, "<br>Max Phos: ", max)
+  ) %>%
+  addLegend(
+    position = "bottomright",
+    colors = "blue",
+    labels = "Count",
+    opacity = 0.7
+  ) %>%
+  addScaleBar()
+
+# Assuming phos_summary has latitude and longitude columns (replace with your actual column names)
+phos_summary_map <- phos_summary %>%
+  filter(!is.na(dec_lat_va) & !is.na(dec_long_va)) %>%
+  leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap tiles
+  addCircleMarkers(
+    lat = ~dec_lat_va,  # Specify the latitude column
+    lng = ~dec_long_va,  # Specify the longitude column
+    radius = ~sqrt(count),  # Adjust the circle size based on count
+    color = ~ifelse(max > 5, "red", "green"),  # Conditional color based on max value
+    fillOpacity = 0.7,       # Circle fill opacity
+    label = ~paste("Site: ", site_no, "<br>Max Phos: ", max),
+    popup = ~paste("Site: ", site_no, "<br>Max Phos: ", max)
+  ) %>%
+  addLegend(
+    position = "bottomright",
+    colors = c("green", "red"),  # Add colors for the legend
+    labels = c("Max Phos <= 5", "Max Phos > 5"),  # Add labels for the legend
+    opacity = 0.7
+  ) %>%
+  addScaleBar()
+
+
